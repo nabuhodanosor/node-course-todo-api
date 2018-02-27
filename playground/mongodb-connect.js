77fce8aa975861887cf4a94a9afa71760f63b338
Yevhen Grinman <yevgirnman@yahoo.com>
2018-02-27 16:33:53 -0500

    Init comit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,37 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server.');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  // db.collection('Users').insertOne({
+  //   name: 'Yevhen',
+  //   age: 34,
+  //   location: 'Rockville'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp()));
+  // });
+
+  db.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server.');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5a95a7f0b3e41a2501fb381c')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({name: 'Yevhen'}).toArray().then((docs) =>{
+    console.log(JSON.stringify(docs, undefined, 2));
+  });
+
+  // db.close();
+});
